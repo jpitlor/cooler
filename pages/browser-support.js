8f00e4930775aa13783ab8a90b4aa43698206928
Jordan Pitlor <jordan@jordanpitlor.com>
2018-04-17 11:30:45 -0400

    injecting the theme should work

--- pages/browser-support.js
+++ pages/browser-support.js
@@ -1,7 +1,10 @@
-chrome.runtime.sendMessage({from: 'cooler-content-script'}, () => {
-	fetch(chrome.extension.getURL('/templates/browser-support.hbs'))
-		.then(r => r.text())
-		.then(d => inject(Handlebars.compile(d)));
+chrome.runtime.sendMessage({from: 'cooler-content-script'});
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+	if (request.from === 'cooler-theme-injected') {
+		fetch(chrome.extension.getURL('/templates/browser-support.hbs'))
+			.then(r => r.text())
+			.then(d => inject(Handlebars.compile(d)));
+	}
 });
 
 function inject(template) {
---
+++ pages/ecommerce-check-in.js
@@ -0,0 +1,10 @@
+chrome.runtime.sendMessage({from: 'cooler-content-script'});
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+	if (request.from === 'cooler-theme-injected') {
+
+	}
+});
+
+function inject(template) {
+
+}
